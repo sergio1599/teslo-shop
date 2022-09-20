e319b4cf85d481c3e36bb87d4444f5743546b4f8
sergio1599 <sergioq15@hotmail.com>
2022-09-20 11:03:57 -0500

    create ShopLayout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import { FC } from "react";
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+  children: JSX.Element | JSX.Element[];
+}
+
+export const ShopLayout: FC<Props> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={pageDescription} />
+        <meta name="og:title" content={title} />
+        <meta name="og:desciption" content={pageDescription} />
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+      </Head>
+      <nav>{/* NavBar */}</nav>
+      {/* SideBar */}
+      <main
+        style={{
+          margin: "80px auto",
+          maxWidth: "1440px",
+          padding: "0px 40px",
+        }}
+      >
+        {children}
+      </main>
+      <footer>{/* Custom footer */}</footer>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { ShopLayout } from "../components/layouts";
+import { Typography } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
-   <h1>
-    Teslo-Shop
-   </h1>
-  )
-}
+    <ShopLayout
+      title={"TesloShop - Home"}
+      pageDescription={"Encuentra los mejores productos aquí"}
+    >
+      <Typography variant="h1" component="h1">
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{mb:1}}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
+  );
+};
 
-export default Home
+export default Home;
