827ae9ceba2152c43a2746617bbb1cd498a6edf0
Sergio Quintana <sergioq15@hotmail.com>
2022-12-28 21:31:13 -0500

    delete address cookies at logout

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -87,11 +87,21 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
   const logout = () => {
     Cookies.remove('token');
     Cookies.remove('cart');
+
+    Cookies.remove('firstName');
+    Cookies.remove('lastName');
+    Cookies.remove('address');
+    Cookies.remove('address2');
+    Cookies.remove('zip');
+    Cookies.remove('city');
+    Cookies.remove('country');
+    Cookies.remove('phone');
+
     router.reload();
   };
 
   return (
-    <AuthContext.Provider value={{ ...state, loginUser, registerUser, logout}}>
+    <AuthContext.Provider value={{ ...state, loginUser, registerUser, logout }}>
       {children}
     </AuthContext.Provider>
   );
